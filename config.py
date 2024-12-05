a15d660c2aa50a84d8d2c3ccec0d16f7f51ca629
Orlando <orlandovillanueva11@gmail.com>
2024-12-05 18:58:00 -0500

    Script update
    Remove chapters that are not primarly aphorims, and improve fairness in verse selection logic.

---
+++ config.py
@@ -0,0 +1,46 @@
+# List of all verse references from Proverbs 10-29
+PROVERBS_VERSES = [
+    # Chapter 10 (32 verses)
+    *[(10, i) for i in range(1, 33)],
+    # Chapter 11 (31 verses)
+    *[(11, i) for i in range(1, 32)],
+    # Chapter 12 (28 verses)
+    *[(12, i) for i in range(1, 29)],
+    # Chapter 13 (25 verses)
+    *[(13, i) for i in range(1, 26)],
+    # Chapter 14 (35 verses)
+    *[(14, i) for i in range(1, 36)],
+    # Chapter 15 (33 verses)
+    *[(15, i) for i in range(1, 34)],
+    # Chapter 16 (33 verses)
+    *[(16, i) for i in range(1, 34)],
+    # Chapter 17 (28 verses)
+    *[(17, i) for i in range(1, 29)],
+    # Chapter 18 (24 verses)
+    *[(18, i) for i in range(1, 25)],
+    # Chapter 19 (29 verses)
+    *[(19, i) for i in range(1, 30)],
+    # Chapter 20 (30 verses)
+    *[(20, i) for i in range(1, 31)],
+    # Chapter 21 (31 verses)
+    *[(21, i) for i in range(1, 32)],
+    # Chapter 22 (29 verses)
+    *[(22, i) for i in range(1, 30)],
+    # Chapter 23 (35 verses)
+    *[(23, i) for i in range(1, 36)],
+    # Chapter 24 (34 verses)
+    *[(24, i) for i in range(1, 35)],
+    # Chapter 25 (28 verses)
+    *[(25, i) for i in range(1, 29)],
+    # Chapter 26 (28 verses)
+    *[(26, i) for i in range(1, 29)],
+    # Chapter 27 (27 verses)
+    *[(27, i) for i in range(1, 28)],
+    # Chapter 28 (28 verses)
+    *[(28, i) for i in range(1, 29)],
+    # Chapter 29 (27 verses)
+    *[(29, i) for i in range(1, 28)],
+]
+
+# Default translation to use
+TRANSLATION = "KJV" 
